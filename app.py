cdee4f14a916208c0204c9ceaefe3297169c4a48
Fazt <fazt@faztweb.com>
2019-07-28 17:56:53 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+from products import products
+
+# Testing Route
+@app.route('/ping', methods=['GET'])
+def ping():
+    return jsonify({'response': 'pong!'})
+
+# Get Data Routes
+@app.route('/products')
+def getProducts():
+    # return jsonify(products)
+    return jsonify({'products': products})
+
+
+@app.route('/products/<string:product_name>')
+def getProduct(product_name):
+    productsFound = [
+        product for product in products if product['name'] == product_name.lower()]
+    if (len(productsFound) > 0):
+        return jsonify({'product': productsFound[0]})
+    return jsonify({'message': 'Product Not found'})
+
+# Create Data Routes
+@app.route('/products', methods=['POST'])
+def addProduct():
+    new_product = {
+        'name': request.json['name'],
+        'price': request.json['price'],
+        'quantity': 10
+    }
+    products.append(new_product)
+    return jsonify({'products': products})
+
+# Update Data Route
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def editProduct(product_name):
+    productsFound = [product for product in products if product['name'] == product_name]
+    if (len(productsFound) > 0):
+        productsFound[0]['name'] = request.json['name']
+        productsFound[0]['price'] = request.json['price']
+        productsFound[0]['quantity'] = request.json['quantity']
+        return jsonify({
+            'message': 'Product Updated',
+            'product': productsFound[0]
+        })
+    return jsonify({'message': 'Product Not found'})
+
+# DELETE Data Route
+@app.route('/products/<string:product_name>', methods=['DELETE'])
+def deleteProduct(product_name):
+    productsFound = [product for product in products if product['name'] == product_name]
+    if len(productsFound) > 0:
+        products.remove(productsFound[0])
+        return jsonify({
+            'message': 'Product Deleted',
+            'products': products
+        })
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
---
+++ products.py
@@ -0,0 +1,5 @@
+products = [
+    {'name': 'laptop', 'price': 800, 'quantity': 4},
+    {'name': 'mouse', 'price': 40, 'quantity': 10},
+    {'name': 'monitor', 'price': 400, 'quantity': 3}
+]
